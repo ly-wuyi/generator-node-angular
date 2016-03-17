7464367cf616e8f1832bee90928e31ac7254bd7a
yong.liu <yong.liu@timanetworks.com>
2016-03-17 11:30:41 +0800

    1. manage the package.json template.

--- app/index.js
+++ app/index.js
@@ -12,28 +12,39 @@ module.exports = generators.Base.extend({
     },
     prompting: function () {
         var done = this.async();
-        this.prompt({
-            type: "input",
-            name: "name",
-            message: "Your project name",
-            default: this.appname
-        },function(answers){
-            this.log(answers.name);
-            this.config.set("projectName", answers.name);
+        var prompts = [
+            {
+                type: "input",
+                name: "appName",
+                message: "Your project name",
+                default: this.appname
+            },
+            {
+                type: "input",
+                name: "appDescription",
+                message: "Your project description",
+                default: "A node and angular module!"
+            }
+        ];
+        this.prompt(prompts, function(answers){
+            this.appName = answers.appName;
+            this.appDescription = answers.appDescription;
             done();
         }.bind(this));
     },
     build: function(){
-        this.appName = "nodeAngular";
-        if (shell.which('git')) {
-            var gitUrl = shell.exec('git config --get remote.origin.url', { silent: true, async: false}).output.trim();
-            if(!gitUrl){
+        var repositoryUrl = "",authorEmail = "";
+        if (shell.which("git")) {
+            repositoryUrl = shell.exec("git config --get remote.origin.url", { silent: true, async: false}).output.trim();
+            authorEmail = shell.exec("git config --get user.email", { silent: true, async: false}).output.trim();
+            if(!repositoryUrl){
                 this.log("Not a git repository!");
             }
-            this.gitUrl = gitUrl;
         }else{
             this.log("The git command is invalid!");
         }
+        this.authorEmail = authorEmail;
+        this.repositoryUrl = repositoryUrl;
         this.template("package.json");
     }
 });
